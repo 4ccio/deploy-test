18a20a6195d0e7021317e564ec481cfc429c51c3
Mikhail Rodionov <misha25025.su@gmail.com>
2024-05-13 19:38:57 +0300

    return to default App jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App.jsx'
+import { App } from './App.jsx'
 import './index.css'
 import { MainPage } from './pages/MainPage.jsx';
 
@@ -24,6 +24,6 @@ import { MainPage } from './pages/MainPage.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <RouterProvider router={router} /> */}
-    <App></App>
+    <App/>
   </React.StrictMode>,
 )
